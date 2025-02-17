2138e26701630c03f8235a478a4ff78905cc613c
gilbruno <gilles_bruno@gmx.fr>
2025-02-17 14:06:51 +0100

    change metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ const geistMono = localFont({
 
 export const metadata: Metadata = {
   title: "InRealArt",
-  description: "Artshow Paris 2025",
+  description: "ArtCapital Paris 2025 - Adélaïde Leferme",
 };
 
 export default async function RootLayout({
