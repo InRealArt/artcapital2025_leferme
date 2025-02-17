47a0261c9e3604e3b2d1c2a19e623f053fc9c8ee
gilbruno <gilles_bruno@gmx.fr>
2025-02-17 11:30:52 +0100

    change testimonials

--- src/components/Testimonials/Testimonials.tsx
+++ src/components/Testimonials/Testimonials.tsx
@@ -21,12 +21,12 @@ const Testimonials = () => {
     </div>
     <div className={styles.containerTestimonials}>
       <div className={styles.containerTestimonial1}>
-        <Image src="/img/testimonials/fboucheix.jpg" 
-          alt="François Boucheix - Artiste contemporain TOP 4 I-CAC - Témoignage sur InRealArt" 
+        <Image src="/img/testimonials/nadine_leprince.jpg" 
+          alt="Nadine Le Prince - Artiste contemporain TOP 3 I-CAC - Témoignage sur InRealArt" 
           width={150} height={150} className={styles.testimonialImage} />
         <div className={styles.wrapperText2}>
           <div className={styles.title2}>
-            <div className={styles.artist1}>François Boucheix</div>
+            <div className={styles.artist1}>Nadine Le Prince</div>
           </div>
           <div className={styles.artistText1}>
               <span className={styles.artistText11}>
